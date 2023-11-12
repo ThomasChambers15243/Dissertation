eaff93bdfb9e9189df8981b2cb7cf65b1d9ed946
TomChambers <tc262389@falmouth.ac.uk>
2023-11-12 14:38:08 +0000

    Created Generation.py and moved testing to there

---
+++ Generation.py
@@ -0,0 +1,22 @@
+import os
+import openai
+
+openai.api_key = os.environ['API_KEY']
+
+MODEL = "gpt-3.5-turbo"
+
+def GetResponce(content):
+
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+        messages=[
+            {
+            "role": "user", "content": content },
+            {"role": "user", "content": "Any code you are asked to write, return only the code. No explenation or tests of the code. No extra notes. The first line of code should be the first function in the programn"}
+        ],
+        temperature=0,
+    )
+
+    code = response['choices'][0]['message']['content']
+
+    return code
