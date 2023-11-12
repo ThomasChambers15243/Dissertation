19a4c281925bfff2b6d1f9d8f07b8588f278c6e2
TomChambers <tc262389@falmouth.ac.uk>
2023-11-12 19:12:09 +0000

    First Steps of Analyzer

---
+++ TestFile.py
@@ -0,0 +1,13 @@
+def TEST(s):
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in mapping:
+            if not stack or stack[-1] != mapping[char]:
+                return False
+            stack.pop()
+        else:
+            stack.append(char)
+
+    return len(stack) == 0
--- main.py
+++ main.py
@@ -1,20 +1,4 @@
 import Generation
 
-# openai.api_key = os.environ['API_KEY']
-#
-# MODEL = "gpt-3.5-turbo"
-#
-# response = openai.ChatCompletion.create(
-#     model=MODEL,
-#     messages=[
-#         {
-#         "role": "user", "content": "Given a string s containing just the characters '(', ')', '{', '}', ' [' and ']', determine if the input string is valid. An input string is valid if: Open brackets must be closed by the same type of brackets. Open brackets must be closed in the correct order. Every close bracket has a corresponding open bracket of the same type. Name the function TEST1" },
-#         {"role": "user", "content": "Any code you are asked to write, return only the code. No explenation or tests of the code. No extra notes. The first line of code should be the first function in the programn"}
-#     ],
-#     temperature=0,
-# )
-# code = response['choices'][0]['message']['content']
-
-code = Generation.GetResponce("Given a string s containing just the characters '(', ')', '{', '}', ' [' and ']', determine if the input string is valid. An input string is valid if: Open brackets must be closed by the same type of brackets. Open brackets must be closed in the correct order. Every close bracket has a corresponding open bracket of the same type. Name the function TEST1")
-
-print(code)
+# p1 = Generation.GetResponce("Given a string s containing just the characters '(', ')', '{', '}', ' [' and ']', determine if the input string is valid. An input string is valid if: Open brackets must be closed by the same type of brackets. Open brackets must be closed in the correct order. Every close bracket has a corresponding open bracket of the same type. Name the function P1")
+# p2 = Generation.GetResponce("You are given a non-negative floating-point number rounded to two decimal places celsius, that denotes the temperature in Celsius. You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit]. Name the function P2")
