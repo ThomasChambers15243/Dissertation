189a6a1e4985e63c564bd916f74f64d3b9bb7a73
TomChambers <tc262389@falmouth.ac.uk>
2023-11-15 15:32:03 +0000

    Added Analyzer import and method call into main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import Generation
-
+import Analyzer
 # p1 = Generation.GetResponce("Given a string s containing just the characters '(', ')', '{', '}', ' [' and ']', determine if the input string is valid. An input string is valid if: Open brackets must be closed by the same type of brackets. Open brackets must be closed in the correct order. Every close bracket has a corresponding open bracket of the same type. Name the function P1")
 # p2 = Generation.GetResponce("You are given a non-negative floating-point number rounded to two decimal places celsius, that denotes the temperature in Celsius. You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit]. Name the function P2")
+
+Analyzer.CalculateAllHalsteadMetrics("TestFile.py")
