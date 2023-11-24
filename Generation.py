34a476cee6d08e33e51c8027d33b579340085220
TomChambers <tc262389@falmouth.ac.uk>
2023-11-24 17:51:44 +0000

    Updated prompt

--- Generation.py
+++ Generation.py
@@ -13,7 +13,7 @@ def GetResponce(content, temperature=0.6):
         messages=[
             {
             "role": "user", "content": content },
-            {"role": "user", "content": "Any code you are asked to write, return only the code. No explenation or tests of the code. No extra notes. The first line of code should be the first function in the programn"}
+            {"role": "user", "content": "Any code you are asked to write, return only the code. No explenation or tests of the code. No extra notes. All of the responce should be able to immediately run. The first line of code should be the first function in the programn"}
         ],
         temperature=temperature,
     )
