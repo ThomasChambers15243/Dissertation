7946e4081940bd66b868dafd4610c80ace2b8825
TomChambers <tc262389@falmouth.ac.uk>
2023-11-24 19:24:54 +0000

    Organised main script

--- main.py
+++ main.py
@@ -1,14 +1,58 @@
 import Generation
 import Analyzer
+import csv
+import json
 
+# Create CSV File with appropriate headers
+with open('results.csv', 'w', newline='') as file:
+   writer = csv.writer(file)
+   writer.writerow(["Problem", "Distinct Operators", "Distinct Operands", "Total Operators", "Total Operands", "Vocabulary", "Length", "Estimated Program Length", "Volume", "Difficulty", "Effort", "Time", "Bugs Estimate"])
+
+# Initialize Research Parameters
+problems = json.load(open('problems.json'))
+problemAmount = 6
+k = 1
 temperature = 0.6
 
-#p1 = Generation.GetResponce("Given a string s containing just the characters '(', ')', '{', '}', ' [' and ']', determine if the input string is valid. An input string is valid if: Open brackets must be closed by the same type of brackets. Open brackets must be closed in the correct order. Every close bracket has a corresponding open bracket of the same type. Name the function P1", temperature)
-#p2 = Generation.GetResponce("You are given a non-negative floating-point number rounded to two decimal places celsius, that denotes the temperature in Celsius. You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit]. Name the function P2", temperature)
-#p6 = Generation.GetResponce("Given two arrays of strings, combine them into one sorted array, with the shortest length string at index 0 and the longest length string at the last index. If two strings are the same length, sum up each string’s characters’ ASCII value, and use that total, inserting the smallest first. Return the new array. Name the function P6.", temperature)
-#print(P6(["a", "cc", "aaaaaa"],["bb","bbb"]))
+# Generate Solutions
+pNumber = 1
+for p in problems:
+    for i in range(1, k+1):
+        with open(f"Solutions/problem{pNumber}/p{pNumber}-{i}.py", "w") as file:
+            response = Generation.GetResponce(problems[p], temperature)
+            file.write(response)
+    pNumber += 1
 
-#with open("TestFile.py", "w") as file:
-   #file.write(p1)
-Analyzer.CalculateAllHalsteadMetrics("TestFile.py")
 
+
+# For each problem, calculate the  average metrics for each sample to the solution
+for solution in range(1, problemAmount+1):
+    # Dictionary to hold the running total and average of the metrics
+    totalMetrics = {
+        "distinctOperatorCount": 0,
+        "distinctOperandCount": 0,
+        "totalOperatorCount": 0,
+        "totalOperandCount": 0,
+        "vocab": 0,
+        "length": 0,
+        "eProgLength": 0,
+        "volume": 0,
+        "difficulty": 0,
+        "effort": 0,
+        "time": 0,
+        "bugsEstimate": 0}
+    # For each Solution to that problem
+    for i in range(1, k+1):
+        metrics = Analyzer.CalculateAllHalsteadMetrics(f"Solutions/problem{solution}/p{solution}-{i}.py")
+        # Add the metrics to the running total
+        for key, values in metrics.items():
+            totalMetrics[key] += values
+
+    # Average the metrics
+    for key, values in totalMetrics.items():
+        totalMetrics[key] = values / k
+
+    # Write the average metrics to a csv file
+    with open("results.csv", mode='a',newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([solution, totalMetrics["distinctOperatorCount"], totalMetrics["distinctOperandCount"], totalMetrics["totalOperatorCount"], totalMetrics["totalOperandCount"], totalMetrics["vocab"], totalMetrics["length"], totalMetrics["eProgLength"], totalMetrics["volume"], totalMetrics["difficulty"], totalMetrics["effort"], totalMetrics["time"], totalMetrics["bugsEstimate"]])
