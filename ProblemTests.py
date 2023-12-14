9faf75f11a391695ee05fa256d7878914227063b
TomChambers <tc262389@falmouth.ac.uk>
2023-12-14 10:23:36 +0000

    Refactored unit tests

--- ProblemTests.py
+++ ProblemTests.py
@@ -21,32 +21,20 @@ class TestP1(unittest.TestCase):
 
 class TestP2(unittest.TestCase):
     def test_valid_brackets(self):
-        self.assertTrue(P2("()"))
-        self.assertTrue(P2("[]"))
-        self.assertTrue(P2("{}"))
-        self.assertTrue(P2("()[]{}"))
-        self.assertTrue(P2("{[()]}"))
+        valid = ["()", "[]", "{}", "()[]{}", "{[()]}"]
+        for i in valid:
+            self.assertTrue(P2(i))
     def test_invalid_brackets(self):
-        self.assertFalse(P2("(("))
-        self.assertFalse(P2("))"))
-        self.assertFalse(P2("({"))
-        self.assertFalse(P2(")}"))
-        self.assertFalse(P2("]["))
-        self.assertFalse(P2("}{"))
-        self.assertFalse(P2("({["))
-        self.assertFalse(P2("]})"))
-        self.assertFalse(P2("({[)}]"))
+        invalid = ["((","))","({","})","][","}{","({[","]})","({[)}]"]
+        for i in invalid:
+            self.assertFalse(P2(i))
 
 class TestP3(unittest.TestCase):
-    def test_longest_sequence(self):
-        self.assertEqual(P3(""), 0)
-        self.assertEqual(P3("a"), 1)
-        self.assertEqual(P3("abcdefg"), 1)
-        self.assertEqual(P3("aaa"), 3)
-        self.assertEqual(P3("abcdeeeefghi"), 4)
-        self.assertEqual(P3("bbbbba"), 5)
-        self.assertEqual(P3("ef3{'as;d12909999999"), 7)
-        self.assertEqual(P3("01001000 01101001 00111010 00101001"), 3)
+    def test_valid_longest_sequence(self):
+        valid = [("",0),("a",1),("abcdefg",1),("aaa",3),("abcdeeeefghi",4),("bbbbba",5),
+                 ("ef3{'as;d12909999999",7),("01001000 01101001 00111010 00101001",3)]
+        for i in valid:
+                self.assertEqual(P3(i[0]), i[1])
 
 class TestP4(unittest.TestCase):
     def test_duplicate_strings(self):
@@ -57,32 +45,32 @@ class TestP4(unittest.TestCase):
 
 class TestP5(unittest.TestCase):
     def test_sort_array(self):
-        self.assertEqual(P5([]), [])
-        self.assertEqual(P5([3, 1, 2]), [1, 2, 3])
-        self.assertEqual(P5([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
-        self.assertEqual(P5([1, 1, 1, 1, 1]), [1, 1, 1, 1, 1])
-        self.assertEqual(P5([1]), [1])
-        self.assertEqual(P5([-1, -2, -3]), [-3, -2, -1])
+        valid = [([3,1,2],[1,2,3]),([5,4,3,2,1],[1,2,3,4,5]),
+                 ([1,1,1,1,1],[1,1,1,1,1]),([1],[1]),
+                 ([-1,-2,-3],[-3,-2,-1])]
+        for i in valid:
+            self.assertEqual(P5(i[0]), i[1])
     def test_large_array(self):
-        large_array = [i for i in range(-2**16, 2**16)]
+        large_array = list(range(-2**16, 2**16))
         shuffled = random.sample(large_array, len(large_array))
         self.assertEqual(P5(shuffled), large_array)
 
 class TestP6(unittest.TestCase):
     def test_combine_and_sort_arrays(self):
-        self.assertEqual(P6([], []), [])
-        self.assertEqual(P6(["a","aa","aaa","aaaa","aaaaa",], ["b","bb","bbb","bbbb", "bbbbb"]), ["a","b","aa","bb","aaa","bbb","aaaa","bbbb","aaaaa","bbbbb"])
-        self.assertEqual(P6(["abc", "de"], []), ["de", "abc"])
-        self.assertEqual(P6([], ["f", "ghi"]), ["f", "ghi"])
-        self.assertEqual(P6(["abc", "de"], ["f", "ghi"]), ["f", "de", "abc", "ghi"])
-        self.assertEqual(P6(["abc", "de"], ["f", "gh"]), ["f", "de", "gh", "abc"])
+        valid = [([],[],[]),(["a","aa","aaa","aaaa","aaaaa",],["b","bb","bbb","bbbb", "bbbbb"],
+                            ["a","b","aa","bb","aaa","bbb","aaaa","bbbb","aaaaa","bbbbb"]),
+                            (["abc", "de"],[],["de", "abc"]),([],["f", "ghi"],["f", "ghi"]),
+                            (["abc", "de"], ["f", "ghi"],["f", "de", "abc", "ghi"]),
+                            (["abc", "de"], ["f", "gh"],["f", "de", "gh", "abc"])]
+        for i in valid:
+            self.assertEqual(P6(i[0],i[1]), i[2])
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(TestP1('test_temperature_conversion'))
     suite.addTest(TestP2('test_valid_brackets'))
     suite.addTest(TestP2('test_invalid_brackets'))
-    suite.addTest(TestP3('test_longest_sequence'))
+    suite.addTest(TestP3('test_valid_longest_sequence'))
     suite.addTest(TestP4('test_duplicate_dtrings'))
     suite.addTest(TestP5('test_sort_array'))
     suite.addTest(TestP5('test_large_array'))
