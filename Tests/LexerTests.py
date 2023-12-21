5e14707e7f6e5120f4c67d715fd58eb32f704d09
TomChambers <tc262389@falmouth.ac.uk>
2023-12-21 12:55:49 +0000

    Added comments of tests classes

--- Tests/LexerTests.py
+++ Tests/LexerTests.py
@@ -1,6 +1,7 @@
 import unittest
 from Code import Lexer
 
+# Tests against an emtpy file
 class TestEmpty(unittest.TestCase):
     def test_BlankInput(self):
         operator, operand = Lexer.TokeniseCode("LexerTestSamples/EmtpyFile.py")
@@ -9,6 +10,7 @@ class TestEmpty(unittest.TestCase):
         self.assertEqual(operand[0], 0)
         self.assertEqual(len(operand[1]), 0)
 
+# Tests against a file with only comments
 class TestOnlyComments(unittest.TestCase):
     def test_OnlyComments(self):
         operator, operand = Lexer.TokeniseCode("LexerTestSamples/OnlyComments.py")
@@ -17,6 +19,7 @@ class TestOnlyComments(unittest.TestCase):
         self.assertEqual((operand[0]), 0)
         self.assertEqual(len(operand[1]), 0)
 
+# Tests against a file with only operators,
 # Operators are all normal operators, keywords and brackets of all kinds ( (), [], {} )
     # Currently fails due to lexer not catching operators such as '+='
     # It splits them up into '+' '='
@@ -25,6 +28,7 @@ class TestOnlyOperators(unittest.TestCase):
     def test_OnlyOperatorsUnique(self):
         return True
         with open("LexerTestSamples/OnlyOperators.py", "w") as file:
+            # Writes most up-to-date OPERATORS
             for key, value in Lexer.OP_TABLE.items():
                 file.writelines(key)
                 file.writelines('\n')
@@ -32,6 +36,7 @@ class TestOnlyOperators(unittest.TestCase):
         self.assertEqual(operator[0], 69)
         self.assertEqual(len(operator[1]),69)
 
+# Tests against a file with only operands,
 # Operands are variables, methods, constants (False, True, strings and other data type values)
 class TestOnlyOperands(unittest.TestCase):
     def test_OnlyOperandsUnique(self):
@@ -47,6 +52,7 @@ def suite():
     suite.addTest(TestOnlyOperators("test_OnlyOperatorsUnique"))
     suite.addTest(TestOnlyOperands("test_OnlyOperandsUnique"))
     suite.addTest(TestOnlyComments("test_OnlyComments"))
+
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
     testRun = runner.run(suite())
