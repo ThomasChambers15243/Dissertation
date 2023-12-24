e5a65ae405d45210d751d0d24785319025617269
TomChambers <tc262389@falmouth.ac.uk>
2023-12-24 18:10:23 +0000

    Added Implementations to Gather Class

--- Code/Gather.py
+++ Code/Gather.py
@@ -17,7 +17,7 @@ class Gather:
         self.RAW_RESULTS_CSV_FILE_PATH = RAW_RESULTS_CSV_FILE_PATH
         self.SAMPLE_RESULTS_CSV_FILE_PATH = SAMPLE_RESULTS_CSV_FILE_PATH
         self.SOLUTIONS_FILE_PATH = SOLUTIONS_FILE_PATH
-        self.PROBLEMS_FILE_PATH = PROBLEMS_FILE_PATH
+        self.PROBLEMS = json.load(open(PROBLEMS_FILE_PATH))
 
         # Research Parameters
         self.PROBLEM_AMOUNT = PROBLEM_AMOUNT        
@@ -29,27 +29,98 @@ class Gather:
 
     # One method to gather all data from the proposed samples
     def GetData(self):
+        for temperature in self.TEMPERATURE_RANGES:
+            # Remove any files from solutions
+            self.InnitSolutionsFolder()
+
+            # Generate Solutions to problems at given temperature,
+            # writing them to files
+            self.GenerateSolutions(temperature)
+
+            # Pass at K here
+
+            # Calculate average scores and write them to a csv for each sample/problem
+            for problemNumber, problem in enumerate(self.PROBLEMS):
+                totalSumMetrics = self.SumMetricScores(f"{self.SOLUTIONS_FILE_PATH}problem{problemNumber}/")
+                totalSumMetrics = self.CalculateAverageMetric(totalSumMetrics)
+
+                # Write metric average to csv
+
+
         raise NotImplementedError
+
 
     # Cleans the Solutions Folder
     def InnitSolutionsFolder(self):
-        raise NotImplementedError
+        # Checks if the folder exists, if so, wipes the contents so the study starts anew
+        for i in range(self.PROBLEM_AMOUNT):
+            if os.path.exists(f"{self.SOLUTIONS_FILE_PATH}problem{i}"):
+                # Wipe content of folder
+                for file in os.listdir(f"{self.SOLUTIONS_FILE_PATH}problem{i}"):
+                    os.remove(f"{self.SOLUTIONS_FILE_PATH}problem{i}/{file}")
+            else:
+                # Create the folder
+                os.mkdir(f"{self.SOLUTIONS_FILE_PATH}problem{i}")
 
-    # Generates solutions
-    def GenerateSolutions(self, solutionsfilePath, desiredTemperature, numOfIterations):
-        raise NotImplementedError
+    # Sets up headers for results .csv
+    def InnitCSV(self, csvPath):
+        # Create CSV File with appropriate headers
+        with open(f"{csvPath}", 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Problem", "Distinct Operators", "Distinct Operands", "Total Operators", "Total Operands",
+                             "Vocabulary", "Length", "Estimated Program Length", "Volume", "Difficulty", "Effort",
+                             "Time", "Bugs Estimate"])
+
+    # Generates solutions for all problems at
+    # given temperature
+    # given number of iterations
+    def GenerateSolutions(self, temperature):
+        for problemNumber, problem in enumerate(self.PROBLEMS):
+            for i in range(self.k_iterations):
+                with open(f"{self.SOLUTIONS_FILE_PATH}problem{problemNumber}/generated--{i}.py", "w") as file:
+                    response = Generation.GetResponce(self.PROBLEMS[problem], temperature)
+                    file.write(response)
+
+    # Given a folder with solutions,
+    # returns one dictionary with the total
+    # metrics of each solution
+    def SumMetricScores(self, problemFolderPath):
+        # Dictionary to hold the sum total of metrics
+        totalMetrics = {
+            "distinctOperatorCount": 0,
+            "distinctOperandCount": 0,
+            "totalOperatorCount": 0,
+            "totalOperandCount": 0,
+            "vocab": 0,
+            "length": 0,
+            "eProgLength": 0,
+            "volume": 0,
+            "difficulty": 0,
+            "effort": 0,
+            "time": 0,
+            "bugsEstimate": 0}
+
+        # For each Solution to that problem
+        for i in range(self.k_iterations):
+            metrics = self.CalculateMetrics(f"{problemFolderPath}generated--{i}.py")
+            # Add the metrics to the running total
+            for key, values in metrics.items():
+                totalMetrics[key] += values
+
+        return totalMetrics
+
+    # Calculates average metric scores
+    def CalculateAverageMetric(self, totalMetrics):
+        # Calculates the mean for each metric
+        for key, values in totalMetrics.items():
+            totalMetrics[key] = values / self.k_iterations
+        return totalMetrics
+
 
     # Given a code file, return the metric scores as a dictionary
     # Returns a Dictionary
-    def CalculateMetric(self, solutionFilePath):
-        raise NotImplementedError
-
-    # Given a list of metric dictionary scores,
-    # calculates the mean of each value.
-    # Returns a dictionary
-
-    def CalculateAverageMetric(self,solutionFilePath):
-        raise NotImplementedError
+    def CalculateMetrics(self, solutionFilePath):
+        return Analyzer.CalculateAllHalsteadMetrics(solutionFilePath)
 
     def CalculateSampleScore(self):
         raise NotImplementedError
