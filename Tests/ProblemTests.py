63a81d76ace5364f3ec1e0e33ee2b1e67888f55d
TomChambers <tc262389@falmouth.ac.uk>
2024-01-10 12:44:47 +0000

    Added reload module feature to testing so that it works on the most recent file

--- Tests/ProblemTests.py
+++ Tests/ProblemTests.py
@@ -1,5 +1,6 @@
 import unittest
 import random
+import importlib
 from Tests import MethodTestFile
 
 # How to test each method in each test file?
@@ -9,6 +10,7 @@ from Tests import MethodTestFile
 
 class TestP1(unittest.TestCase):
     def test_temperature_conversion(self):
+        importlib.reload(MethodTestFile)
         self.assertEqual(MethodTestFile.P1(0), [273.15, 32.00])
         self.assertEqual(MethodTestFile.P1(100), [373.15, 212.00])
         self.assertEqual(MethodTestFile.P1(-273.15), [0.00, -459.67])
@@ -16,17 +18,21 @@ class TestP1(unittest.TestCase):
 
 class TestP2(unittest.TestCase):
     def test_valid_brackets(self):
+        importlib.reload(MethodTestFile)
         valid = ["()", "[]", "{}", "()[]{}", "{[()]}"]
         for i in valid:
             self.assertTrue(MethodTestFile.P2(i))
     def test_invalid_brackets(self):
+        importlib.reload(MethodTestFile)
         invalid = ["((","))","({","})","][","}{","({[","]})","({[)}]"]
         for i in invalid:
             self.assertFalse(MethodTestFile.P2(i))
 
 
+
 class TestP3(unittest.TestCase):
     def test_valid_longest_sequence(self):
+        importlib.reload(MethodTestFile)
         valid = [("",0),("a",1),("abcdefg",1),("aaa",3),("abcdeeeefghi",4),("bbbbba",5),
                  ("ef3{'as;d12909999999",7),("01001000 01101001 00111010 00101001",3)]
         for i in valid:
@@ -34,6 +40,7 @@ class TestP3(unittest.TestCase):
 
 class TestP4(unittest.TestCase):
     def test_duplicate_strings(self):
+        importlib.reload(MethodTestFile)
         self.assertEqual(MethodTestFile.P4(["a"]), 1)
         self.assertEqual(MethodTestFile.P4(["a", "b", "c", "d", "e"]), 5)
         self.assertEqual(MethodTestFile.P4(["a", "b", "c", "d", "e", "a", "b", "c", "d", "e"]), 5)
@@ -41,12 +48,14 @@ class TestP4(unittest.TestCase):
 
 class TestP5(unittest.TestCase):
     def test_sort_array(self):
+        importlib.reload(MethodTestFile)
         valid = [([3,1,2],[1,2,3]),([5,4,3,2,1],[1,2,3,4,5]),
                  ([1,1,1,1,1],[1,1,1,1,1]),([1],[1]),
                  ([-1,-2,-3],[-3,-2,-1])]
         for i in valid:
             self.assertEqual(MethodTestFile.P5(i[0]), i[1])
     def test_large_array(self):
+        importlib.reload(MethodTestFile)
         large_array = list(range(-2**16, 2**16))
         shuffled = random.sample(large_array, len(large_array))
         self.assertEqual(MethodTestFile.P5(shuffled), large_array)
