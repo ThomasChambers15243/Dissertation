c9481bb419e2b85a9cbc687e3c631b881c9a4d20
TomChambers <tc262389@falmouth.ac.uk>
2024-03-07 13:26:31 +0000

    Finished ComplexityTests.py

--- Tests/ComplexityTests.py
+++ Tests/ComplexityTests.py
@@ -2,15 +2,17 @@ import unittest
 from config import PATHS
 from Code import mccabe
 
+TEST_NAME = "Complexity"
 
-class TestEmpty(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
+class TestScores(unittest.TestCase):
+    def test_zero_value(self):
+        self.assertEqual(0, mccabe.GetTotalValue(f"{PATHS['PYTHON_FILE_TEST_SAMPLES']}EmtpyFile.py"))
+        self.assertEqual(0, mccabe.GetTotalValue(f"{PATHS['PYTHON_FILE_TEST_SAMPLES']}OnlyComments.py"))
+    def test_scripts(self):
+        self.assertEqual(0, mccabe.GetTotalValue(f"{PATHS['PYTHON_FILE_TEST_SAMPLES']}ExampleScript1.py"))
+        self.assertEqual(1, mccabe.GetTotalValue(f"{PATHS['PYTHON_FILE_TEST_SAMPLES']}ExampleScript2.py"))
+        self.assertEqual(9, mccabe.GetTotalValue(f"{PATHS['PYTHON_FILE_TEST_SAMPLES']}ExampleScript3.py"))
 
 def RunTests():
-    Suite = unittest.TestLoader.loadTestsFromTestCase(TestEmpty)
-    #Suite.addTests(unittest.TestLoader.loadTestsFromTestCase())
+    Suite = unittest.TestLoader().loadTestsFromTestCase(TestScores)
     return unittest.TextTestRunner().run(Suite)
-
-RunTests()
